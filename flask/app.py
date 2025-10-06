e1e4c67ab606f0f96faa41f85a2aeda9ca895761
Hamizah <23ftt1821@student.pb.edu.bn>
2025-10-06 21:15:10 +0800

    add import string in app.py

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify, abort, string
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify, abort
+import string
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
 from datetime import timedelta
