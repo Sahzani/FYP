5a0800f1841d21954a5f79ca056c3d02b9605939
Nursahira Izani <23FTT1825@student.pb.edu.bn>
2025-10-06 15:52:12 +0800

    added camera

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify, abort
 import firebase_admin
 from firebase_admin import credentials, auth, firestore
 from datetime import timedelta
